e038bb696fcb8ff84d58ea6bcd0e943dff04a42d
Jusufadis Bakamovic <jbakam@gmail.com>
2017-02-24 21:02:10 +0100

    Remove '__visit_all_nodes()'. Implement 'dump_ast_nodes()' in terms of traversal interface.

--- core/services/parser/clang_parser.py
+++ core/services/parser/clang_parser.py
@@ -253,53 +253,49 @@ class ClangParser():
                 'Cursor.Extent %-25s' % ('[' + str(cursor.extent.start.line) + ', ' + str(cursor.extent.start.column) + ']:[' + str(cursor.extent.end.line) + ', ' + str(cursor.extent.end.column) + ']'))
 
     def dump_ast_nodes(self, filename):
-        if filename not in self.tunits:
-            return
+        def visitor(ast_node, ast_parent_node, client_data):
+            if ast_node.location.file and ast_node.location.file.name == filename:  # we're only interested in symbols from given file
+                # if ast_node.kind in [clang.cindex.CursorKind.CALL_EXPR, clang.cindex.CursorKind.MEMBER_REF_EXPR]:
+                #    self.dump_tokens(ast_node)
 
-        logging.debug('%-12s' % '[Line, Col]' + '%-40s' % 'Spelling' + '%-40s' % 'Kind' + '%-40s' % 'Type.Spelling' +
-                '%-40s' % 'Type.Kind' +
-                '%-40s' % 'OverloadedDecl' + '%-40s' % 'NumOverloadedDecls' +
-                '%-40s' % 'Referenced.Spelling' + '%-40s' % 'Referenced.Kind' +
-                '%-40s' % 'Referenced.Type.Spelling' + '%-40s' % 'Referenced.Type.Kind' +
-                '%-40s' % 'Referenced.ResultType.Spelling' + '%-40s' % 'Referenced.ResultType.Kind' +
-                '%-40s' % 'Referenced.Canonical.Spelling' + '%-40s' % 'Referenced.Canonical.Kind' +
-                '%-40s' % 'Referenced.SemanticParent.Spelling' + '%-40s' % 'Referenced.SemanticParent.Kind' +
-                '%-40s' % 'Referenced.LexicalParent.Spelling' + '%-40s' % 'Referenced.LexicalParent.Kind')
-        logging.debug('----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+                logging.debug(
+                    '%-12s' % ('[' + str(ast_node.location.line) + ', ' + str(ast_node.location.column) + ']') +
+                    '%-40s' % str(ast_node.spelling) +
+                    '%-40s' % str(ast_node.kind) +
+                    '%-40s' % str(ast_node.type.spelling) +
+                    '%-40s' % str(ast_node.type.kind) +
+                    ('%-40s' % str(ClangParser.__get_overloaded_decl(ast_node, 0).spelling) if (ast_node.kind ==
+                        clang.cindex.CursorKind.OVERLOADED_DECL_REF and ClangParser.__get_num_overloaded_decls(ast_node)) else '%-40s' % '-') +
+                    ('%-40s' % str(ClangParser.__get_overloaded_decl(ast_node, 0).kind) if (ast_node.kind ==
+                        clang.cindex.CursorKind.OVERLOADED_DECL_REF and ClangParser.__get_num_overloaded_decls(ast_node)) else '%-40s' % '-') +
+                    ('%-40s' % str(ast_node.referenced.spelling) if (ast_node.referenced) else '%-40s' % '-') +
+                    ('%-40s' % str(ast_node.referenced.kind) if (ast_node.referenced) else '%-40s' % '-') +
+                    ('%-40s' % str(ast_node.referenced.type.spelling) if (ast_node.referenced) else '%-40s' % '-') +
+                    ('%-40s' % str(ast_node.referenced.type.kind) if (ast_node.referenced) else '%-40s' % '-') +
+                    ('%-40s' % str(ast_node.referenced.result_type.spelling) if (ast_node.referenced) else '%-40s' % '-') +
+                    ('%-40s' % str(ast_node.referenced.result_type.kind) if (ast_node.referenced) else '%-40s' % '-') +
+                    ('%-40s' % str(ast_node.referenced.canonical.spelling) if (ast_node.referenced) else '%-40s' % '-') +
+                    ('%-40s' % str(ast_node.referenced.canonical.kind) if (ast_node.referenced) else '%-40s' % '-') +
+                    ('%-40s' % str(ast_node.referenced.semantic_parent.spelling) if (ast_node.referenced and ast_node.referenced.semantic_parent) else '%-40s' % '-') +
+                    ('%-40s' % str(ast_node.referenced.semantic_parent.kind) if (ast_node.referenced and ast_node.referenced.semantic_parent) else '%-40s' % '-') +
+                    ('%-40s' % str(ast_node.referenced.lexical_parent.spelling) if (ast_node.referenced and ast_node.referenced.lexical_parent) else '%-40s' % '-') +
+                    ('%-40s' % str(ast_node.referenced.lexical_parent.kind) if (ast_node.referenced and ast_node.referenced.lexical_parent) else '%-40s' % '-'))
 
-        for idx, cursor in enumerate(self.tunits[filename].ast_node_list):
+            return ChildVisitResult.RECURSE.value
 
-            # if cursor.kind in [clang.cindex.CursorKind.CALL_EXPR, clang.cindex.CursorKind.MEMBER_REF_EXPR]:
-            #    self.dump_tokens(cursor)
 
-            logging.debug(
-                '%-12s' % ('[' + str(cursor.location.line) + ', ' + str(cursor.location.column) + ']') +
-                '%-40s' % str(cursor.spelling) +
-                '%-40s' % str(cursor.kind) +
-                '%-40s' % str(cursor.type.spelling) +
-                '%-40s' % str(cursor.type.kind) +
-                ('%-40s' % str(ClangParser.__get_overloaded_decl(cursor, 0).spelling) if (cursor.kind ==
-                    clang.cindex.CursorKind.OVERLOADED_DECL_REF and ClangParser.__get_num_overloaded_decls(cursor)) else '%-40s' % '-') +
-                ('%-40s' % str(ClangParser.__get_overloaded_decl(cursor, 0).kind) if (cursor.kind ==
-                    clang.cindex.CursorKind.OVERLOADED_DECL_REF and ClangParser.__get_num_overloaded_decls(cursor)) else '%-40s' % '-') +
-                ('%-40s' % str(cursor.referenced.spelling) if (cursor.referenced) else '%-40s' % '-') +
-                ('%-40s' % str(cursor.referenced.kind) if (cursor.referenced) else '%-40s' % '-') +
-                ('%-40s' % str(cursor.referenced.type.spelling) if (cursor.referenced) else '%-40s' % '-') +
-                ('%-40s' % str(cursor.referenced.type.kind) if (cursor.referenced) else '%-40s' % '-') +
-                ('%-40s' % str(cursor.referenced.result_type.spelling) if (cursor.referenced) else '%-40s' % '-') +
-                ('%-40s' % str(cursor.referenced.result_type.kind) if (cursor.referenced) else '%-40s' % '-') +
-                ('%-40s' % str(cursor.referenced.canonical.spelling) if (cursor.referenced) else '%-40s' % '-') +
-                ('%-40s' % str(cursor.referenced.canonical.kind) if (cursor.referenced) else '%-40s' % '-') +
-                ('%-40s' % str(cursor.referenced.semantic_parent.spelling) if (cursor.referenced and cursor.referenced.semantic_parent) else '%-40s' % '-') +
-                ('%-40s' % str(cursor.referenced.semantic_parent.kind) if (cursor.referenced and cursor.referenced.semantic_parent) else '%-40s' % '-') +
-                ('%-40s' % str(cursor.referenced.lexical_parent.spelling) if (cursor.referenced and cursor.referenced.lexical_parent) else '%-40s' % '-') +
-                ('%-40s' % str(cursor.referenced.lexical_parent.kind) if (cursor.referenced and cursor.referenced.lexical_parent) else '%-40s' % '-'))
-
-    def __visit_all_nodes(self, node, original_filename):
-        for n in node.get_children():
-            if n.location.file and n.location.file.name == self.tunits[original_filename].contents_filename:
-                self.tunits[original_filename].ast_node_list.append(n)
-                self.__visit_all_nodes(n, original_filename)
+        if filename in self.tunits:
+            logging.debug('%-12s' % '[Line, Col]' + '%-40s' % 'Spelling' + '%-40s' % 'Kind' + '%-40s' % 'Type.Spelling' +
+                    '%-40s' % 'Type.Kind' +
+                    '%-40s' % 'OverloadedDecl' + '%-40s' % 'NumOverloadedDecls' +
+                    '%-40s' % 'Referenced.Spelling' + '%-40s' % 'Referenced.Kind' +
+                    '%-40s' % 'Referenced.Type.Spelling' + '%-40s' % 'Referenced.Type.Kind' +
+                    '%-40s' % 'Referenced.ResultType.Spelling' + '%-40s' % 'Referenced.ResultType.Kind' +
+                    '%-40s' % 'Referenced.Canonical.Spelling' + '%-40s' % 'Referenced.Canonical.Kind' +
+                    '%-40s' % 'Referenced.SemanticParent.Spelling' + '%-40s' % 'Referenced.SemanticParent.Kind' +
+                    '%-40s' % 'Referenced.LexicalParent.Spelling' + '%-40s' % 'Referenced.LexicalParent.Kind')
+            logging.debug('----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+            self.traverse(self.tunits[filename].cursor, None, visitor)
 
     @staticmethod
     def __extract_dependent_type_kind(cursor):
