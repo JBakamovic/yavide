6305eb47a5afc56cf51f2c5eba6f44ee4747aca0
Jusufadis Bakamovic <jbakam@gmail.com>
2017-02-27 16:34:08 +0100

    Make 'ClangIndexer' be in charge of having up-to-date AST.
    * This makes all other services behave as AST consumers only without
      worrying about rebuilding the AST.

--- core/services/syntax_highlighter/syntax_highlighter.py
+++ core/services/syntax_highlighter/syntax_highlighter.py
@@ -7,16 +7,6 @@ class SyntaxHighlighter():
         self.callback = callback
 
     def __call__(self, args):
-        contents_filename = str(args[0])
-        original_filename = str(args[1])
-        compiler_args = list(str(args[2]).split())
-        project_root_directory = str(args[3])
-        if contents_filename != original_filename:
-            start = time.clock()
-            self.parser.run(contents_filename, original_filename, compiler_args, project_root_directory)
-            time_elapsed = time.clock() - start
-            logging.info("Parsing '{0}' took {1}.".format(original_filename, time_elapsed))
-
         if self.callback:
             self.callback(self.parser, args)
 
