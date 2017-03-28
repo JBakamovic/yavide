c84a9129ef9c149ff511bf9b4edb4f061bcfc6a7
Jusufadis Bakamovic <jbakam@gmail.com>
2017-03-28 10:44:49 -0400

    Avoid using magic numbers when invoking the callback.

--- core/services/indexer/clang_indexer.py
+++ core/services/indexer/clang_indexer.py
@@ -19,30 +19,30 @@ class ClangIndexer():
         }
 
     def __call__(self, args):
-        self.op.get(int(args[0]), self.__unknown_op)(args[1:len(args)])
+        self.op.get(int(args[0]), self.__unknown_op)(int(args[0]), args[1:len(args)])
 
-    def __unknown_op(self, args):
-        logging.error("Unknown operation triggered! Valid operations are: {0}".format(self.op))
+    def __unknown_op(self, id, args):
+        logging.error("Unknown operation with ID={0} triggered! Valid operations are: {1}".format(id, self.op))
 
-    def __load_from_disk(self, args):
+    def __load_from_disk(self, id, args):
         start = time.clock()
         success = self.parser.load_from_disk(str(args[0]))
         time_elapsed = time.clock() - start
         logging.info("Loading from {0} took {1}.".format(str(args[0]), time_elapsed))
 
         if self.callback:
-            self.callback(0x0, success)
+            self.callback(id, success)
 
-    def __save_to_disk(self, args):
+    def __save_to_disk(self, id, args):
         start = time.clock()
         success = self.parser.save_to_disk(str(args[0]))
         time_elapsed = time.clock() - start
         logging.info("Saving to {0} took {1}.".format(str(args[0]), time_elapsed))
 
         if self.callback:
-            self.callback(0x1, success)
+            self.callback(id, success)
 
-    def __run_on_single_file(self, args):
+    def __run_on_single_file(self, id, args):
         proj_root_directory = str(args[0])
         contents_filename = str(args[1])
         original_filename = str(args[2])
@@ -70,9 +70,9 @@ class ClangIndexer():
         logging.info("Indexing {0} took {1}.".format(original_filename, time_elapsed))
 
         if self.callback:
-            self.callback(0x2, args)
+            self.callback(id, args)
 
-    def __run_on_directory(self, args):
+    def __run_on_directory(self, id, args):
         proj_root_directory = str(args[0])
         compiler_args = list(str(args[1]).split())
         logging.info("Indexing a whole project '{0}' ... ".format(proj_root_directory))
@@ -102,19 +102,19 @@ class ClangIndexer():
         logging.info("Indexing {0} took {1}.".format(proj_root_directory, time_elapsed))
 
         if self.callback:
-            self.callback(0x3, args)
+            self.callback(id, args)
 
-    def __drop_single_file(self, args):
+    def __drop_single_file(self, id, args):
         self.parser.drop_translation_unit(str(args[0]))
         if self.callback:
-            self.callback(0x4, args)
+            self.callback(id, args)
 
-    def __drop_all(self, dummy = None):
+    def __drop_all(self, id, dummy = None):
         self.parser.drop_all_translation_units()
         if self.callback:
-            self.callback(0x5, dummy)
+            self.callback(id, dummy)
 
-    def __go_to_definition(self, args):
+    def __go_to_definition(self, id, args):
         cursor = self.parser.get_definition(str(args[0]), int(args[1]), int(args[2]))
         if cursor:
             logging.info('Definition location %s' % str(cursor.location))
@@ -122,9 +122,9 @@ class ClangIndexer():
             logging.info('No definition found.')
 
         if self.callback:
-            self.callback(0x10, cursor.location if cursor else None)
+            self.callback(id, cursor.location if cursor else None)
 
-    def __find_all_references(self, args):
+    def __find_all_references(self, id, args):
         start = time.clock()
         references = self.parser.find_all_references(str(args[0]), int(args[1]), int(args[2]))
         time_elapsed = time.clock() - start
@@ -133,4 +133,4 @@ class ClangIndexer():
             logging.info("Ref location %s" % str(r))
 
         if self.callback:
-            self.callback(0x11, references)
+            self.callback(id, references)
