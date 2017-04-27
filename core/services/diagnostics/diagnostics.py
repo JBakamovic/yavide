1ec3fd54c402759ddda53f2a4386040fa86fd9dc
Jusufadis Bakamovic <jbakam@gmail.com>
2017-04-27 21:26:25 +0200

    Make diagnostic interface conform to the new design (indexer).

--- core/services/diagnostics/diagnostics.py
+++ core/services/diagnostics/diagnostics.py
@@ -2,11 +2,12 @@ import logging
 import time
 
 class Diagnostics():
-    def __init__(self, parser, callback = None):
+    def __init__(self, tunit_pool, parser, callback = None):
+        self.tunit_pool = tunit_pool
         self.parser = parser
         self.callback = callback
 
     def __call__(self, args):
-        diagnostics_iter = self.parser.get_diagnostics(str(args[0]))
+        diagnostics_iter = self.parser.get_diagnostics(self.tunit_pool[str(args[0])])
         if self.callback and diagnostics_iter:
             self.callback(diagnostics_iter, args)
