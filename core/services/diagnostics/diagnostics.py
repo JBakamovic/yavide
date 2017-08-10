c6f42e8ab74f47691a3d8cbb17116b070648b66b
Jusufadis Bakamovic <jbakam@gmail.com>
2017-08-10 14:25:02 +0200

    Refactoring: make diagnostics play with new arch.

--- core/services/diagnostics/diagnostics.py
+++ core/services/diagnostics/diagnostics.py
@@ -2,12 +2,24 @@ import logging
 import time
 
 class Diagnostics():
-    def __init__(self, tunit_pool, parser, callback = None):
-        self.tunit_pool = tunit_pool
+    def __init__(self, parser, callback = None):
         self.parser = parser
         self.callback = callback
 
     def __call__(self, args):
-        diagnostics_iter = self.parser.get_diagnostics(self.tunit_pool[str(args[0])])
-        if self.callback and diagnostics_iter:
-            self.callback(diagnostics_iter, args)
+        project_root_directory = str(args[0])
+        contents_filename = str(args[1])
+        original_filename = str(args[2])
+        compiler_args = str(args[3])
+
+        tunit = self.parser.parse(
+            contents_filename,
+            original_filename,
+            compiler_args,
+            project_root_directory
+        )
+
+        if tunit is not None:
+            diagnostics_iter = self.parser.get_diagnostics(tunit)
+            if self.callback and diagnostics_iter:
+                self.callback(diagnostics_iter, args)
