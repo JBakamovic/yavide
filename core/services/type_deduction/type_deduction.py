f8179f7dbd5fe4b3165fedabe7efefcc536b3abd
Jusufadis Bakamovic <jbakam@gmail.com>
2017-08-10 14:45:57 +0200

    Refactoring: make type-deduction play with new arch.

--- core/services/type_deduction/type_deduction.py
+++ core/services/type_deduction/type_deduction.py
@@ -1,10 +1,24 @@
 class TypeDeduction():
-    def __init__(self, tunit_pool, parser, callback = None):
-        self.tunit_pool = tunit_pool
+    def __init__(self, parser, callback = None):
         self.parser = parser
         self.callback = callback
 
     def __call__(self, args):
-        cursor = self.parser.map_source_location_to_cursor(self.tunit_pool[str(args[0])], int(args[1]), int(args[2]))
-        if self.callback and cursor and cursor.type:
-            self.callback(cursor.type.spelling, args)
+        project_root_directory = str(args[0])
+        contents_filename = str(args[1])
+        original_filename = str(args[2])
+        compiler_args = str(args[3])
+        line = int(args[4])
+        column = int(args[5])
+
+        tunit = self.parser.parse(
+            contents_filename,
+            original_filename,
+            compiler_args,
+            project_root_directory
+        )
+
+        if tunit is not None:
+            cursor = self.parser.map_source_location_to_cursor(tunit, line, column)
+            if self.callback and cursor and cursor.type:
+                self.callback(cursor.type.spelling, args)
