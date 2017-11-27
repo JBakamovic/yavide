f48d2108f3bd80e91cff8cc0ecc15a99692143f9
Jusufadis Bakamovic <jbakam@gmail.com>
2017-11-27 23:14:54 +0100

    Make caching strategy a configurable parameter.

--- core/services/parser/tunit_cache.py
+++ core/services/parser/tunit_cache.py
@@ -1,5 +1,26 @@
 import os
 
+class UnlimitedCache():
+    def __init__(self):
+        self.store = {}
+
+    def __getitem__(self, key):
+        return self.store[key]
+
+    def __setitem__(self, key, value):
+        if key in self.store:
+            del self.tunit[tunit_filename]
+        self.store[key] = value
+
+    def __delitem__(self, key):
+        del self.store[key]
+
+    def __iter__(self):
+        return self.store.__iter__()
+
+    def __len__(self):
+        return len(self.store)
+
 class NoneTranslationUnitCache():
     def __init__(self):
         pass
@@ -10,51 +31,33 @@ class NoneTranslationUnitCache():
     def insert(self, tunit_filename, tunit):
         pass
 
-    def drop(self, tunit_filename):
-        pass
+    def __setitem__(self, key, item):
+        self.insert(key, item)
 
-    def clear(self):
-        pass
+    def __getitem__(self, key):
+        return self.fetch(key)
 
     def __len__(self):
         return 0
 
+class TranslationUnitCache():
+    def __init__(self, cache=UnlimitedCache()):
+        self.tunit = cache
+
+    def fetch(self, tunit_filename):
+        if tunit_filename in self.tunit:
+            return self.tunit[tunit_filename]
+        return (None, None,)
+
+    def insert(self, tunit_filename, tunit):
+        self.tunit[tunit_filename] = (tunit, os.path.getmtime(tunit.spelling),)
+
     def __setitem__(self, key, item):
         self.insert(key, item)
 
     def __getitem__(self, key):
         return self.fetch(key)
-
-    def __delitem__(self, key):
-        self.drop(key)
-
-class TranslationUnitCache():
-    def __init__(self):
-        self.tunit = {}
-
-    def fetch(self, tunit_filename):
-        return self.tunit.get(tunit_filename, (None, None,))
-
-    def insert(self, tunit_filename, tunit):
-        self.drop(tunit_filename)
-        self.tunit[tunit_filename] = (tunit, os.path.getmtime(tunit.spelling),)
-
-    def drop(self, tunit_filename):
-        if tunit_filename in self.tunit:
-            del self.tunit[tunit_filename]
-
-    def clear(self):
-        self.tunit.clear()
 
     def __len__(self):
         return len(self.tunit)
-
-    def __setitem__(self, key, item):
-        self.insert(key, item)
-
-    def __getitem__(self, key):
-        return self.fetch(key)
-
-    def __delitem__(self, key):
-        self.drop(key)
 
