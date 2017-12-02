2439d6dc865f980a9c08365b5b58faa836d5569c
Jusufadis Bakamovic <jbakam@gmail.com>
2017-12-02 19:13:07 +0100

    Integrate some more logging & timing into the clang-tidy service.

--- core/services/clang_tidy_service.py
+++ core/services/clang_tidy_service.py
@@ -2,6 +2,7 @@ import logging
 import os
 import subprocess
 import tempfile
+import time
 from services.yavide_service import YavideService
 from common.yavide_utils import YavideUtils
 
@@ -19,11 +20,11 @@ class ClangTidy(YavideService):
         root, ext = os.path.splitext(compilation_database)
         if ext == '.json':  # In case we have a JSON compilation database we simply use one
             self.compiler_options = '-p ' + compilation_database
-            logging.info("Clang-Tidy compiler arguments configured with JSON database.")
+            logging.info("clang-tidy will extract compiler flags from existing JSON database.")
         else:               # Otherwise we provide compilation flags inline
             with open(compilation_database) as f:
                 self.compiler_options = '-- ' + f.read().replace('\n', ' ')
-            logging.info("Clang-Tidy compiler arguments configured inline with '{0}'.".format(self.compiler_options))
+            logging.info("clang-tidy will use compiler flags given inline: '{0}'.".format(self.compiler_options))
         YavideUtils.call_vim_remote_function(self.yavide_instance, "Y_ClangTidy_StartCompleted()")
 
     def __shutdown_callback(self, args):
@@ -34,8 +35,11 @@ class ClangTidy(YavideService):
     def __call__(self, args):
         filename, apply_fixes = args
         cmd = self.cmd + ' ' + filename + ' ' + str('-fix' if apply_fixes else '') + ' ' + self.compiler_options
+        logging.info("Triggering clang-tidy over '{0}' with '{1}'".format(filename, cmd))
         with open(self.output_file.name, 'w') as f:
+            start = time.clock()
             subprocess.call(cmd, shell=True, stdout=f)
-        logging.info("Clang-Tidy over '{0}' completed with '{1}'".format(filename, cmd))
+            end = time.clock()
+        logging.info("Clang-Tidy over '{0}' completed in {1}s.".format(filename, end-start))
         YavideUtils.call_vim_remote_function(self.yavide_instance, "Y_ClangTidy_Apply('" + self.output_file.name + "')")
 
